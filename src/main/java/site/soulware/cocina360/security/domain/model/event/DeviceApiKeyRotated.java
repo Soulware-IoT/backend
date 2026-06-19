074936ee6909e11ee3d0ca8d094708547612737f
overGDev <joaquinorozco2004@gmail.com>
2026-06-19 12:31:56 -0500

    chore: delete orphan files

--- src/main/java/site/soulware/cocina360/security/domain/model/event/DeviceApiKeyRotated.java
+++
@@ -1,8 +0,0 @@
-package site.soulware.cocina360.security.domain.model.event;
-
-import site.soulware.cocina360.shared.domain.model.event.DomainEvent;
-
-import java.time.Instant;
-import java.util.UUID;
-
-public record DeviceApiKeyRotated(UUID deviceId, Instant occurredOn) implements DomainEvent {}
