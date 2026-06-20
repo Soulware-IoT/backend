d71c923e0c19a7b97bb7169f46cd53e63032176a
Henry Reaño <henry29.2004@hotmail.com>
2026-06-19 20:51:00 -0500

    feat: add cors config

---
+++ src/main/java/site/soulware/cocina360/shared/infrastructure/config/WebConfig.java
@@ -0,0 +1,18 @@
+package site.soulware.cocina360.shared.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://backend-production-c5e8.up.railway.app", "http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
